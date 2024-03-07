eee3724a3cfe8433d6ca8a7d6a1a1581f49316c6
Muhammad Mahbub <muhammadmahbub07@gmail.com>
2024-03-07 07:29:48 +0600

    router

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,10 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 import App from './components/App.vue';
+import route from './router/index.js'
+import router from './router/index.js';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router)
+app.mount('#app')
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,9 +1,6 @@
 <template>
-    <div>
-        <h1>Hi vue</h1>
-    </div>
+
+    <router-view></router-view>
 </template>
 
-<script>
-export default {};
-</script>
+
